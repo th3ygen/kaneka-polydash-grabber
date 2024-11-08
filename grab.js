2e98870a1ba1a837ea9f316aaed7dacd8bffe58f
Aidil <th3ygen@gmail.com>
2024-11-08 11:01:58 +0800

    refactor: improve logging and code formatting in grab.js

--- grab.js
+++ grab.js
@@ -1,21 +1,24 @@
-const { exec } = require('child_process');
-const WebSocket = require('ws');
-const schedule = require('node-schedule');
+const { exec } = require("child_process");
+const WebSocket = require("ws");
+const schedule = require("node-schedule");
 
-const wsUrl = 'ws://localhost:7613?role=grabber';
+const wsUrl = "ws://localhost:7613?role=grabber";
 
-const command = "java -cp '/usr/lib/jvm/java-17-oracle/lib/PIJDBCDriver.jar' jdbc.java";
+const command =
+	"java -cp '/usr/lib/jvm/java-17-oracle/lib/PIJDBCDriver.jar' jdbc.java";
 
 const ws = new WebSocket(wsUrl);
 
 const task = () => {
-	console.log('Grabbing data from JDBC...');
+	console.log(`[${new Date().toLocaleString()}] Grabbing data from JDBC...`);
 	exec(command, (err, out, stderr) => {
 		if (err) {
-			console.error(`Error executing command: ${err}`);
+			console.error(
+				`[${new Date().toLocaleString()}] Error executing command: ${err}`
+			);
 		}
 
-		const lines = out.split('\n');
+		const lines = out.split("\n");
 
 		const data = {};
 		const pattern = /(-?\w+)\s+(-?[\d.]+)/;
@@ -31,23 +34,40 @@ const task = () => {
 
 		if (data["Driver"]) delete data["Driver"];
 
-		console.log(`[${new Date().toLocaleTimeString()}] Data grabbed! =>`, JSON.stringify(data, null, 2));
+		console.log(
+			`[${new Date().toLocaleString()}] Data event:`,
+			JSON.stringify(data).length > 0 ? `${data} bytes` : "Not found"
+		);
 
 		if (ws && ws.readyState === WebSocket.OPEN) {
 			ws.send(JSON.stringify(data));
-			console.log(`[${new Date().toLocaleTimeString()}] Pushed a payload to websocket!`);
+			console.log(`[${new Date().toLocaleString()}] Pushed data`);
 		} else {
-			console.error("WebSocket not connected! data not sent");
+			console.error(
+				`[${new Date().toLocaleString()}] WebSocket not connected`
+			);
 		}
 	});
 };
 
-ws.on('open', () => {
-	console.log('Connected to websocket server!');
-	
-	const job = schedule.scheduleJob('*/1 * * * *', task);
-	
-	if (job) console.log("Job started!");
+// console log the purpose of this script
+// starts with a title and introduction
+console.log("JDBC Grabber");
+console.log(
+	"This script grabs data from a JDBC connection and sends it to a WebSocket server."
+);
+console.log("The data is grabbed every minute.");
+ws.on("open", () => {
+	console.log(
+		`[${new Date().toLocaleString()}] Connected to websocket server!`
+	);
+
+	const job = schedule.scheduleJob("*/1 * * * *", task);
+
+	if (job) console.log(`[${new Date().toLocaleString()}] Job started!`);
 	task();
 });
 
+ws.on('error', (err) => {
+	console.error(`[${new Date().toLocaleString()}] WebSocket error: ${err}`);
+});
